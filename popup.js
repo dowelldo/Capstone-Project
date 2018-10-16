8dc902a99093ae35fc6b9dc20741339081574ae3
dowelldo <31323475+dowelldo@users.noreply.github.com>
2018-10-16 01:11:21 -0400

    Added more javascript

--- popup.js
+++ popup.js
@@ -1,12 +1,20 @@
 var site;
+
+//Set site variable equal to the current URL in the chrome URL field:
 chrome.tabs.query({ 'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT },
     function(tabs) {
         site = tabs[0].url;
     }
 )
+
+//Add a listener to the filter button
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("go").addEventListener("click", clicked);
 });
+
+//function that gets called when the button gets clicked
 function clicked() {
-    document.write(site);
+    var words = document.getElementById("filter_words").value;
+    var swearwords = document.getElementById("swear_words").checked;
+    document.write(swearwords);
 }
